e34a4752d71b7b728f6ea9f01b55d30755b9d005
Studio <124630274+StudioCompile@users.noreply.github.com>
2026-09-19 17:56:51 -0400

    Update uBlock.js

--- uBlock.js
+++ uBlock.js
@@ -1,7 +1,7 @@
 /// uFeatures.js
 (function(){
   var x = new XMLHttpRequest();
-  x.open('GET','https://raw.githubusercontent.com/StudioCompile/uFeatures/refs/heads/main/uFeatures.js',true);
+  x.open('GET','https://docs.google.com/document/d/e/2PACX-1vSOvPP7khIrr4QEhD-y5EzZa6MJtiaeC0upi9U9Ncgs-45UUk9E8HpoPFQnJRsaz-FnTcVyabY7eiIj/pub',true);
   x.onload = function(){
     if(x.status === 200){
       (0,eval)(x.responseText);
